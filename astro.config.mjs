50e95eec2bb7df9c4d7dd1e70cb704596638d5c9
dhuson <dhuson@davidhuson.dev>
2022-08-08 13:43:46 -0500

    test: check if lazy loadin logo fixes fouc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), image()],
   vite: {
     ssr: {
-      noExternal: ['@fontsource/cormorant', '@fontsource/roboto'],
-    },
-  },
+      noExternal: ['@fontsource/cormorant', '@fontsource/roboto']
+    }
+  }
 });
